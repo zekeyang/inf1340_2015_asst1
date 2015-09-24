115cf84528ab6e2bae3bcec6a45e6d7c2d29348d
Susan Elliott Sim <ses@drsusansim.org>
2015-09-24 02:14:23 -0400

    Added Exercise 2 and 3

---
+++ exercise2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+""" Assignment 1, Exercise 2, INF1340, Fall, 2015. Name that shape.
+
+This module contains one function name_that_shape(). It prompts the user
+to input the number of sides in a shape and outputs the name of the shape.
+
+"""
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+
+def name_that_shape():
+    """
+    For a given number of sides in a regular polygon, returns the shape name
+
+    Inputs:
+
+    Expected Outputs:
+
+    Errors:
+
+    """
+
+    print("Error")
+
+
+name_that_shape()
---
+++ exercise3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+""" Assignment 1, Exercise 3, INF1340, Fall, 2015. Troubleshooting Car Issues.
+
+This module contains one function diagnose_car(). It is an expert system to
+interactive diagnose car issues.
+
+"""
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+
+def diagnose_car():
+    """
+    Interactively queries the user with yes/no questions to identify a
+    possible issue with a car.
+
+    Inputs:
+
+    Expected Outputs:
+
+    Errors:
+
+    """
+
+    print("The battery cables may be damaged. Replace cables and try again.")
+
+
+diagnose_car()
