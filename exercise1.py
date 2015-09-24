214c6d4c20c12674e7fe1596eeb98b80e1bd3ef8
Susan Elliott Sim <ses@drsusansim.org>
2015-09-24 00:49:24 -0400

    Added Exercise 1

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+
+This module prints the amount of money that Lakshmi has remaining
+after the stock transactions
+
+"""
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+
+money = 1000.00
+print(money)
